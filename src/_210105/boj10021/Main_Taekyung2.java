16905c260cdc75ce2394bab63bd06beccb93621b
ltk3934 <ltk3934@daum.net>
2021-01-05 14:35:38 +0900

    [TK]2021.01.05 UPDATE

--- src/_210105/boj10021/Main_Taekyung2.java
+++ src/_210105/boj10021/Main_Taekyung2.java
@@ -1,32 +1,21 @@
 package _210105.boj10021;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main_Taekyung2 {
 
-    static class Pair {
-        int first, second;
-
-        public Pair(int first, int second) {
-            this.first = first;
-        this.second = second;
-    }
-}
     static int V, C;
     static int[] x, y;
-    static ArrayList<Pair>[] adj;
+    static int[][] adj;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         V = sc.nextInt();
         C = sc.nextInt();
-        adj = new ArrayList[V];
+        adj = new int[V][V];
         x = new int[V];
         y = new int[V];
-        for(int i = 0; i < V; i++)
-            adj[i] = new ArrayList<>();
         for(int i = 0; i < V; i++) {
             x[i] = sc.nextInt();
             y[i] = sc.nextInt();
@@ -36,8 +25,8 @@ public class Main_Taekyung2 {
             for(int j = i + 1; j < V; j++) {
                 int d = (int) (Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2));
                 if(C <= d) {
-                    adj[i].add(new Pair(j, d));
-                    adj[j].add(new Pair(i, d));
+                    adj[i][j] = d;
+                    adj[j][i] = d;
                 }
             }
         }
@@ -46,26 +35,27 @@ public class Main_Taekyung2 {
 
     static int prim() {
         boolean[] added = new boolean[V];
-        int[] minWeight = new int[V];
-        Arrays.fill(minWeight, Integer.MAX_VALUE);
+        int[] min = new int[V];
+        Arrays.fill(min, Integer.MAX_VALUE);
         int ret = 0;
-        minWeight[0] = 0;
+        min[0] = 0;
         for(int iter = 0; iter < V; iter++) {
             int u = -1;
             for(int v = 0; v < V; v++)
-                if(!added[v] && (u == -1 || minWeight[u] > minWeight[v]))
+                if(!added[v] && (u == -1 || min[u] > min[v]))
                     u = v;
-            ret += minWeight[u];
+            ret += min[u];
             added[u] = true;
 
-            for(int i = 0; i < adj[u].size(); i++) {
-                int v = adj[u].get(i).first, weight = adj[u].get(i).second;
-                if(!added[v] && minWeight[v] > weight) {
-                    minWeight[v] = weight;
+            for(int i = 0; i < V; i++) {
+                if(adj[u][i] == 0) continue;
+                int w = adj[u][i];
+                if(!added[i] && min[i] > w) {
+                    min[i] = w;
                 }
             }
         }
-        for(int n : minWeight) {
+        for(int n : min) {
             if(n == Integer.MAX_VALUE)
                 return -1;
         }
