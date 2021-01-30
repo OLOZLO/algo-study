fc99adc411886067408eb3f04e2d6a3be55f460d
girawhale <girawhale@naver.com>
2021-01-30 22:07:39 +0900

    [SH] 21.01.30 UPDATE 백준 풀이 추가, 주석도 달았음

--- src/_210202/prog17676/Main_girawhale.java
+++ src/_210202/prog17676/Main_girawhale.java
@@ -9,28 +9,32 @@ import java.util.Optional;
 class Main_girawhale {
     public int solution(String[] lines) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
-        int[][] timetable = new int[lines.length][2];
+        int[][] timetable = new int[lines.length][2]; //시작시간, 끝시간으로 저장
 
         for (int i = 0; i < lines.length; i++) {
             String[] split = lines[i].split(" ");
 
-            int end = (int) (LocalTime.parse(split[1], dtf).toNanoOfDay() / 1_000_000);
-            int T = (int) (Double.parseDouble(split[2].substring(0, split[2].length() - 1)) * 1000);
-            int start = end - T + 1;
-            if (start < 0) start = 0;
+            int end = (int) (LocalTime.parse(split[1], dtf).toNanoOfDay() / 1_000_000); // 시간을 모두 초로 변경.
+                                                                                        // 1나노초 = 10^9초, 0이 6개 필요없으니까 없애버림
+            int T = (int) (Double.parseDouble(split[2].substring(0, split[2].length() - 1)) * 1000); // 0.001초를 1로 변환했으니까 1000을 곱해줌
+            int start = end - T + 1; //종료시간이 주어지니까 시작시간을 소요시간에서 빼서 구해주는데
+            if (start < 0) start = 0; //전날부터 시작했을수도 있으니까 0보다 작으면 0으로 바꿔준다
 
-            timetable[i] = new int[]{start, end + 1000};
+            timetable[i] = new int[]{start, end + 1000}; // 1초간 처리량 구한다니까 걍 미리 늘려서 계산 쉽게 하자...
         }
 
         int max = 0;
 //        int[] cnt = new int[24 * 60 * 60 * 1000]; // 190~327ms, 390MB
         Map<Integer, Integer> map = new HashMap<>(); // 8~1200ms, 53~480MB
-        for (int i = 0; i < lines.length; i++) {
+        for (int i = 0; i < lines.length; i++) { // 걍 로그 다 돌기
+            // 로그 돌 때마다 내가 있는 있는 위치에 +1, 그러면 해당 초 때 돌고있는 트래픽수가 저장됨
+            // 종료횟수에 +1000을 했으니까 다음날로 넘어갈 수 있어 하루 최대 초로 Min해줌
             for (int t = timetable[i][0]; t < Math.min(timetable[i][1], 24 * 60 * 60 * 1000); t++) {
 //                max = Math.max(++cnt[t], max);
+
+                // put해주면 기존에 있던 값 나오는데 없을때 put해주면 null나오니까 그거 막을라고 이랬어여...
                 max = Math.max(max, Optional.ofNullable(map.put(t, map.getOrDefault(t, 0) + 1)).orElse(0));
             }
-
         }
 
         return max;
--- src/_210202/prog17678/Main_girawhale.java
+++ src/_210202/prog17678/Main_girawhale.java
@@ -6,33 +6,35 @@ import java.util.TreeMap;
 
 public class Main_girawhale {
     public String solution(int N, int T, int M, String[] timetable) {
+        //String형태 시간을 모두 분형태로 바꿈
         int[] times = Arrays.stream(timetable).mapToInt(t ->
                 Integer.parseInt(t.substring(0, 2)) * 60
                         + Integer.parseInt(t.substring(3, 5))).toArray();
 
-        TreeMap<Integer, Integer> timeCnt = new TreeMap<>();
+        // 정렬 가능한 TreeMap으로 시간순 정렬하고, 나온 횟수 저장
+        TreeMap<Integer, Integer> timeCnt = new TreeMap<>(); // <줄 선 시간, 해당 시간에 줄선 사람 수>
         for (int time : times)
             timeCnt.put(time, timeCnt.getOrDefault(time, 0) + 1);
 
-        int time = 9 * 60;
+        int time = 9 * 60; // 젤 처음은 9시 운행함
 
-        for (int n = 0; n < N; n++) {
-            int busTime = n * T + 9 * 60;
-            int cnt = 0;
+        for (int n = 0; n < N; n++) { // 셔틀 운행 가능횟수만큼 반복
+            int busTime = n * T + 9 * 60; // 현재 셔틀 운행하는 시간
+            int cnt = 0; // 지금 운행하는 버스에 탄사람!
 
-            Map.Entry<Integer, Integer> entry = null;
-            while (!timeCnt.isEmpty() && busTime >= timeCnt.firstKey()) {
+            Map.Entry<Integer, Integer> entry = null; // 운행하는 버스에 마지막으로 탄 entry
+            while (!timeCnt.isEmpty() && busTime >= timeCnt.firstKey()) { // 운행 시간보다 먼저 서야 버스에 탈 수 잇슴
                 entry = timeCnt.pollFirstEntry();
-                cnt += entry.getValue();
+                cnt += entry.getValue(); // 줄 선사람 일단 다 셈
 
-                if (cnt >= M) {
-                    timeCnt.put(entry.getKey(), cnt - M);
-                    break;
+                if (cnt >= M) { // 근데 M보다 크면 못태우니까
+                    timeCnt.put(entry.getKey(), cnt - M); // 넘친만큼 다시 빼서 map에 넣어주고
+                    break; //ㅌㅌ
                 }
             }
-            if (n == N - 1) {
-                if (cnt >= M) time = entry.getKey() - 1;
-            } else time += T;
+            if (n == N - 1) { // 마지막 버스인가요?
+                if (cnt >= M) time = entry.getKey() - 1; // 근데 사람도 많이 탓으면 마지막으로 탄사람보다 먼저 서야되여...
+            } else time += T; // 아니면 젤 늦게 서두댐~
         }
 
         return String.format("%02d:%02d", time / 60, time % 60);
