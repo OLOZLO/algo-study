96640b71e19480a3a9df0dae033c5f20b4e8a6ba
정진아 <human00024@naver.com>
2021-04-05 13:35:05 +0900

    [JA] 21.03.25 HW UPDATE

--- src/_210325/boj20207/Main_ja.java
+++ src/_210325/boj20207/Main_ja.java
@@ -1,15 +1,11 @@
 package _210325.boj20207;
-
 import java.util.Scanner;
 
-public class Main_ja{
+public class Main_ja {
 	/**
-	 * [ 달력 ]
-	 * 1-365
+	 * [ 달력 ] 1-365
 	 * 
-	 * 코팅지를 붙일거야
-	 * - 연속된 두 일자에 각각 일정에 1개 이상 있으면, 연속된 일정
-	 * 	 -> 하나의 직사각형에 포함
+	 * 코팅지를 붙일거야 - 연속된 두 일자에 각각 일정에 1개 이상 있으면, 연속된 일정 -> 하나의 직사각형에 포함
 	 * 
 	 */
 	public static void main(String[] args) {
@@ -20,29 +16,24 @@ public class Main_ja{
 			int start = in.nextInt();
 			int end = in.nextInt();
 			for (int j = start; j <= end; j++) {
-				day[j]+=1;
+				day[j] += 1;
 			}
 		}
 		int result = 0;
 		for (int i = 1; i < 366; i++) {
-			if(day[i] > 0) {
+			if (day[i] > 0) {
 				int col = 0;
 				int max = Integer.MIN_VALUE;
-				for (int j = i; j < 366; j++) {
-					if(day[j] > 0) {
-						col++;
-						max = Math.max(max, day[j]);
-					}
-					else {
-						result += col*max;
-						i=j;
-						break;
-					}
+				while (i < 366 && day[i] != 0) {
+					col++;
+					max = Math.max(max, day[i++]);
 				}
+				
+				result +=max*col;
 			}
 		}
 		System.out.println(result);
-		
+
 	}
-	
+
 }
