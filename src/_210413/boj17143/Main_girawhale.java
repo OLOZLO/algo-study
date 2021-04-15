ebcf9d3593b84783df71d6d956030ea5f61d5789
girawhale <girawhale@naver.com>
2021-04-16 01:29:01 +0900

    [SH] 21.04.16 과제 업로드

---
+++ src/_210413/boj17143/Main_girawhale.java
@@ -0,0 +1,105 @@
+package _210413.boj17143;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/*
+1. 결과 : 성공
+2. 시간 복잡도 : O(C * M * (1000 % Math.max(R, C)))
+                < O(100 * 10^4 * (10^3 % 100))
+                < O(10^6 * 100) = O(10^8) 
+    - 이유 : 낚시왕이 움직이는 거리 = C <= 100
+            상어의 수 = M <= 10^4
+            상어 한마리 당 움직이는 최대 거리 = 1000 % Math.max(R, C)
+            
+            상어가 이동하고 먹히는 과정은 더하기 연산이므로 제외시켜도 무관
+
+3. 풀이 : 단순 시뮬레이션. 직접 상어를 움직이고 낚시를 해 끝까지 이동하면 되는 문제
+*/
+public class Main_girawhale {
+    static int[] dy = {0, -1, 1, 0, 0};
+    static int[] dx = {0, 0, 0, 1, -1};
+    static int[] dir = {0, 2, 1, 4, 3}; // 뒤집어줄 방향 저장
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Shark[][] map = new Shark[R + 1][C + 1];
+        int[] mod = {0, 2 * R - 2, 2 * C - 2}; // 상하로 움직일때, 좌우로 움직일때 이만큼 이동하면 다시 원상복구된다!
+
+        for (int i = 0; i < M; i++) {
+            Shark shark = new Shark(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray());
+            shark.speed %= mod[(shark.dir + 1) / 2]; // mod값만큼 이동하면 처음 위치로 돌아오기 때문에 나머지만큼만 이동하도록 설정
+            map[shark.y][shark.x] = shark;
+        }
+
+        int pos = 0; // 낚시왕의 x좌표
+        int ans = 0; // 낚시한 상어무게
+
+        while (pos < C) {
+            pos++;
+
+            // 낚시왕이 위치한 열에서 아래로 내리면서 null이 아니라면 상어가 존재
+            // 위치한 상어의 무게를 답에 더하고 map을 비우자!
+            int y = 1;
+            while (y <= R && map[y][pos] == null) y++;
+            if (y <= R && map[y][pos] != null) {
+                ans += map[y][pos].size; // 상어 무게 더해준당
+                map[y][pos] = null; // map에 상어가 사라졌어요!
+            }
+
+            Queue<Shark> move = new LinkedList<>(); // 이동한 상어들의 정보들을 담을 Queue
+            for (int i = 1; i <= R; i++) {
+                for (int j = 1; j <= C; j++) {
+                    if (map[i][j] == null) continue; // 상어 없으면 pass
+
+                    Shark shark = map[i][j];
+                    for (int k = 0; k < shark.speed; k++) {
+                        if (shark.dir == 1 && shark.y == 1 || shark.dir == 2 && shark.y == R
+                                || shark.dir == 3 && shark.x == C || shark.dir == 4 && shark.x == 1) {
+                            shark.dir = dir[shark.dir]; // 상어 이동 방향마다 벽에 박으면 뒤집어주기
+                        }
+
+                        shark.y += dy[shark.dir];
+                        shark.x += dx[shark.dir];
+                    }
+
+                    map[i][j] = null; // 상어가 이동했으니까 좌표 비워주고
+                    move.add(shark); // queue에 넣어 한번에 이동하자
+                }
+            }
+
+            while (!move.isEmpty()) {
+                Shark shark = move.poll();
+
+                if (map[shark.y][shark.x] != null) { // map이 null이 아니면 상어가 존재.
+                    if (map[shark.y][shark.x].size < shark.size) // 큰상어 냅두고 작은상어 없애자
+                        map[shark.y][shark.x] = shark;
+                } else map[shark.y][shark.x] = shark; // 현재 칸에 상어 없으니까 그냥 넣기 ㄱ
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    static class Shark {
+        int y, x, speed, dir, size;
+
+        Shark(int[] info) {
+            y = info[0];
+            x = info[1];
+            speed = info[2];
+            dir = info[3];
+            size = info[4];
+        }
+    }
+}
---
+++ src/_210413/boj1939/Main_girawhale.java
@@ -0,0 +1,88 @@
+package _210413.boj1939;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/*
+1. 결과 : 성공
+2. 시간 복잡도 : O(log10^9 * (V + E))
+                < O(log10^9 * (10^4 + 10^5))
+                = O(10^5)
+    - 이유 : 인접리스트 bfs의 시간복잡도 = O(V+E)
+            이분 탐색의 시간복잡도 = O(logN)
+
+3. 풀이 : 최대값을 먼저 정해두고 탐색이 가능한지 돌리는 bfs를 돌린다
+            정상적으로 탐색이 가능하면 최대값을 늘리고, 아니면 줄이는 이분탐색을 통해 답을 구해보자!
+*/
+public class Main_girawhale {
+    static int N, M, start, dest;
+    static ArrayList<int[]>[] adj;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = parseInt(st);
+        M = parseInt(st);
+
+        adj = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++)
+            adj[i] = new ArrayList<>();
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int n1 = parseInt(st), n2 = parseInt(st), c = parseInt(st);
+
+            adj[n1].add(new int[]{n2, c});
+            adj[n2].add(new int[]{n1, c});
+        }
+        st = new StringTokenizer(br.readLine());
+        start = parseInt(st);
+        dest = parseInt(st);
+
+        int s = 1, e = (int) 1e9; // 최소 1, 최대 10^9
+        int ans = 0;
+        while (s <= e) {
+            int m = (s + e) / 2;
+
+            if (solve(m)) { // 최대가 m일 때, 탐색이 가능하니?
+                ans = m; // 가능하면 답 저장ㄱ
+                s = m + 1; // 범위 늘리고
+            } else e = m - 1; // 안되면 줄여서 다시 해보장
+        }
+
+        System.out.println(ans);
+    }
+
+    static boolean solve(int m) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visit = new boolean[N + 1];
+
+        queue.add(start);
+        visit[start] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            if (cur == dest) return true; // 주어진 값으로 도착점까지 탐색이 가능! true
+
+            for (int[] a : adj[cur]) {
+                if (a[1] < m || visit[a[0]]) continue; // 주어진 값보다 작은 도로는 탐색하지 못함. pass
+
+                visit[a[0]] = true;
+                queue.add(a[0]);
+            }
+        }
+        return false; // 도착 못하고 큐가 비어버림. false반환
+    }
+
+    static int parseInt(StringTokenizer st) {
+        return Integer.parseInt(st.nextToken());
+    }
+
+
+}
