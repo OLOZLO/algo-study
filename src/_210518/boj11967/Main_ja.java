a1bfc8c10241fd97e481cd8c3812f080363a7bc7
정진아 <human00024@naver.com>
2021-05-25 13:52:23 +0900

    [JA] 21.05.18 HW - 불켜기(성공코드)

--- src/_210518/boj11967/Main_ja.java
+++ src/_210518/boj11967/Main_ja.java
@@ -17,16 +17,25 @@ import java.util.Queue;
  *  	- 모르겠습니다...ㅠㅠ
  *  
  * 3. 풀이
+ * 	(1) 해당 위치에서 불을 켤 수 있는 방 탐색
+ * 		(1-1) 불 켜기
+ * 		(1-2) 불을 켠 방 주위(상하좌우)에 이미 방문 했던(visited) 방이 존재하면 qu에 추가 -> 주위에 방문했던 방이 존재하다는 것은, 추후에 방문이 가능하다는 것을 의미
+ * 		
+ * 	(2) 해당 위치에서 상하좌우 탐색
+ * 		(2-1) 불을 켠 방이 존재하면 qu에 추가
+ * 
+ * 4. 메모리초과 풀이
  * 	(1) 더 이상 켜진 스위치가 없을 때 까지 무한 반복
  *		(1-1) (0,0)부터 출발
  *		(1-2) bfs 탐색
  * 			- 해당 위치와 연결된 방 스위치 on
  * 			- 해당 위치에서 상하좌우 이동하기 (단, 스위치가 켜진 방만 이동 가능)
  * 
- * 4. 후기
+ * 5. 후기
  *	- 쉬울 줄 알고 덤볐다가, 문제에서 놓친 부분이 있다는 걸 깨닫고 멘붕이 왔다.. => (1,3)에서 (2,1)방의 스위치를 켰을 때, (2,1)방과 다른 방이 연결되어 이동할 수 있는 경우.
  *	- 그러면, 스위치를 켤 때마다 시작지점에서 출발하쟈! 하다가 메모리 초과가 났다. (눈물) 
- *	- 어떻게 해야할까...
+ *
+ *	(+) 먼가 예외가 있을 것 같은데... 하면서 지우다 쓰다 백번한 듯. 맞아서 당황했다...
  * 
  */
 public class Main_ja {
@@ -47,43 +56,45 @@ public class Main_ja {
 		for (int i = 0; i < M; i++) {
 			input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 			int[] cur = new int[] { input[0] - 1, input[1] - 1 };
-			int[] next = new int[] { input[2] - 1, input[3] - 1 };
+			int[] room = new int[] { input[2] - 1, input[3] - 1 };
 
-			switchPoint[cur[0]][cur[1]].add(next); // 해당 위치에서 연결된 방 저장
+			switchPoint[cur[0]][cur[1]].add(room); // 해당 위치에서 연결된 방 저장
 		}
 
 		int[][] dt = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-		Queue<int[]> qu = new LinkedList<>();
+		
 		boolean[][] turnOn = new boolean[N][N]; // 불 켜짐 여부
+		boolean[][] visited = new boolean[N][N]; // 해당 방 방문 여부
+		
+		Queue<int[]> qu = new LinkedList<>();
+		qu.add(new int[] { 0, 0 });
 		turnOn[0][0] = true;
-
+		
 		int result = 1;
-		while (true) { // 추가로 켜진 스위치가 없을 때 까지 무한 반복
-			int add = result;
-			qu.add(new int[] { 0, 0 });
-			boolean[][] visited = new boolean[N][N]; // 해당 방 방문 여부
+		while (!qu.isEmpty()) {
+			int[] cur = qu.poll();
+			visited[cur[0]][cur[1]] = true;
+			
+			for (int[] room : switchPoint[cur[0]][cur[1]]) { // 해당 위치와 연결된 방 스위치 전부 on
+				if(turnOn[room[0]][room[1]] || visited[room[0]][room[1]]) continue; 
 
-			while (!qu.isEmpty()) {
-				int[] cur = qu.poll();
-				if (!visited[cur[0]][cur[1]]) { 
-					for (int[] next : switchPoint[cur[0]][cur[1]]) { // 해당 위치와 연결된 방 스위치 전부 on
-						if (!turnOn[next[0]][next[1]]) // 추가로 켜진 스위치 count
-							add++;
-						turnOn[next[0]][next[1]] = true;
-					}
+				for (int d = 0; d < 4; d++) { // 주변에 방문한 적 있는 방이 있으면 qu에 추가
+					int i = room[0] + dt[d][0];
+					int j = room[1] + dt[d][1];
+					if (i < 0 || j < 0 || i >= N || j >= N || !visited[i][j]) continue;
+					qu.add(new int[] { room[0], room[1] });
+					break;
 				}
-				visited[cur[0]][cur[1]] = true;
-
-				for (int d = 0; d < 4; d++) { // 상하좌우로 이동
-					int i = cur[0] + dt[d][0];
-					int j = cur[1] + dt[d][1];
-					if (i < 0 || j < 0 || i >= N || j >= N || visited[i][j]) continue;
-					if (turnOn[i][j]) // 스위치가 켜진 방만 이동 가능
-						qu.add(new int[] { i, j });
-				}
+				turnOn[room[0]][room[1]] = true;
+				result++;
 			}
-			if (add == result) break;
-			result = add;
+			
+			for (int d = 0; d < 4; d++) { // 불 켜진 곳으로 이동
+				int i = cur[0] + dt[d][0];
+				int j = cur[1] + dt[d][1];
+				if (i < 0 || j < 0 || i >= N || j >= N || visited[i][j] || !turnOn[i][j]) continue;
+				qu.add(new int[] { i, j });
+			}
 		}
 		System.out.println(result);
 	}
